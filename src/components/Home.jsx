dcc0ebf2ec55a96d768e82bbdda952c9f7331146
Abdullah2000Habib <abd3habib@gmail.com>
2023-03-08 14:34:10 +0200

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HomePage from '../pages/HomePage/HomePage';
+
+class Home extends Component {
+    render() {
+        return (
+            <>
+                <HomePage/>
+            </>
+        );
+    }
+}
+
+export default Home;
